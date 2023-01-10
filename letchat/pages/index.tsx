8fcb93d20e23f65d4094e07ff7fcafe6d707179e
emiliawil <emiliawilliamson@pm.me>
2023-01-10 10:40:24 +0000

    packages and dependencies

--- letchat/pages/index.tsx
+++ letchat/pages/index.tsx
@@ -14,7 +14,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1 className="text-3xl font-bold underline">LetChat</h1>
+        <h1 className="text-3xl font-bold underline text-light-primary">
+          LetChat
+        </h1>
       </main>
     </>
   );

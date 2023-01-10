43a566754ec95f7310701699b51812dc8b7500d0
Julio Velezmoro <julio28ve@gmail.com>
2023-01-10 16:04:31 +0000

    Container Template

--- letchat/components/Container/index.tsx
+++ letchat/components/Container/index.tsx
@@ -1,5 +1,15 @@
-const Container = () => {
-  return <div>Container</div>;
+type ContainerProps = {
+  children: React.ReactNode;
+};
+
+const Container = ({ children }: ContainerProps) => {
+    return (
+   
+      <div className="min-w-[100vw] min-h-[100vh] max-w-[1440px] bg-light-light">
+        {children}
+      </div>
+    
+  );
 };
 
 export default Container;

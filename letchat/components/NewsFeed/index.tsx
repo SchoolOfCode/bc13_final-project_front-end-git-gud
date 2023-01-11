41e0b6abb53bfdbb9e15cacc0ec5a2a5f6e4733e
cowtipping <turbogerbil@gmail.com>
2023-01-11 09:20:40 +0000

    newsfeed

--- letchat/components/NewsFeed/index.tsx
+++ letchat/components/NewsFeed/index.tsx
@@ -1,7 +1,32 @@
 const NewsFeed = () => {
   return (
-    <div>NewsFeed</div>
-  )
-}
+    <>
+      <div className="card-body shadow-xl">
+        <div className="bg-primary">
+          <h2 className="card-title">News Feed</h2>
+          <p>01.01.2023</p>
+        </div>
+
+        <div className="">
+          <div className="">
+            <p>Lisa Landlord</p>
+            <p>Response: Mold under sink</p>
+            <p>09:00 - 25.12.2022</p>
+          </div>
+          <div className="">
+            <p>Lisa Landlord</p>
+            <p>Status Update: Out of the country</p>
+            <p>09:00 - 25.12.2022</p>
+          </div>
+          <div className="">
+            <p>Lisa Landlord</p>
+            <p>Message: Scheduled inspection</p>
+            <p>09:00 - 25.12.2022</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default NewsFeed;
--- letchat/jest.config.js
+++ letchat/jest.config.js
@@ -1,5 +1,5 @@
 // jest.config.js
-import nextJest from "next/jest";
+const nextJest = require("next/jest");
 
 const createJestConfig = nextJest({
   // Provide the path to your Next.js app to load next.config.js and .env files in your test environment

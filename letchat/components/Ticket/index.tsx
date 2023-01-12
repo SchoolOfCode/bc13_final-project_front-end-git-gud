6272cc2a2a590a06b2b429f7a646c4e72c69767a
emiliawil <emiliawilliamson@pm.me>
2023-01-12 12:34:20 +0000

    basic ticket styling

--- letchat/components/Ticket/index.tsx
+++ letchat/components/Ticket/index.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import avatar from "./img/avatar.png";
+import avatar from "../../public/assets/images/img_avatar.png";
 import tickets from "../../data/ticketData";
 
 type ticket = {
@@ -16,25 +16,33 @@ const Ticket = (openTicket: { openTicket: (id: number) => void }) => {
     <>
       {tickets.map((ticket: ticket) => {
         return (
-          <article>
-            <header>
-              <div id="ticket-author">
-                <Image src={avatar} alt="avatar" />
-                <p>{ticket.name}</p>
+          <article className="flex flex-col max-w-lg m-2 shadow-lg bg-[#F0C8C2] rounded-2xl">
+            <header className="flex flex-row justify-between p-2 bg-light-hot rounded-t-2xl">
+              <div id="ticket-author" className="flex flex-row align-center">
+                <Image
+                  src={avatar}
+                  alt="avatar"
+                  className="w-10 h-10 rounded-full"
+                />
+                <p className="leading-10 ml-2">{ticket.name}</p>
               </div>
-              <div id="ticket-info">
+              <div
+                id="ticket-info"
+                className="flex flex-col text-right justify-right"
+              >
                 <h5>Ticket #{ticket.id}</h5>
                 <h4>Subject: {ticket.subject}</h4>
               </div>
             </header>
-            <main>
+            <main className="p-2">
               <div>
                 <p>{ticket.lastMessage}</p>
               </div>
               <button
-                onClick={() => {
-                  openTicket(ticket.id);
-                }}
+                className="bg-light-hot rounded-lg px-2 py-1 mt-2 float-right"
+                // onClick={() => {
+                //   openTicket(ticket.id);
+                // }}
               >
                 Read more
               </button>

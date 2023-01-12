67cd9649779199909d6087a8570927d3154e4cd1
ashdashlee <104494009+ashdashlee@users.noreply.github.com>
2023-01-12 08:59:59 +0000

    navebar update

--- letchat/components/Navbar/index.tsx
+++ letchat/components/Navbar/index.tsx
@@ -1,18 +1,20 @@
 const Navbar = (props: any) => {
   return (
-    <div className="navbar bg-base-100">
-      <div className="flex-1">
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+    <div className="navbar min-h-[8rem] p-6 ">
+      <div className="flex-1 pl-8">
+        <a className="btn bg-light-primary w-20 h-20 border-none hover:opacity-60 hover:bg-light-primary rounded-full"></a>
       </div>
-      <div className="flex-none">
+      <div className="flex-1 justify-end h-20">
         <div className="dropdown dropdown-end">
           <div>
-            <button className="btn">Login</button>
+            <button className="btn border-none w-35 h-10 bg-light-secondary hover:opacity-60 hover:bg-light-secondary text-black mr-6">
+              Login
+            </button>
           </div>
         </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-            <div className="w-10 rounded-full">
+            <div className="w-12 rounded-full">
               <img src="https://placeimg.com/80/80/people" />
             </div>
           </label>

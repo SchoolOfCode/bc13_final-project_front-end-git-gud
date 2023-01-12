c9e58898580b06cae17b85c8395022939056114e
emiliawil <emiliawilliamson@pm.me>
2023-01-12 13:59:22 +0000

    ticket styling adjustments, time and date

--- letchat/components/Ticket/index.tsx
+++ letchat/components/Ticket/index.tsx
@@ -16,38 +16,40 @@ const Ticket = (openTicket: { openTicket: (id: number) => void }) => {
     <>
       {tickets.map((ticket: ticket) => {
         return (
-          <article className="flex flex-col max-w-lg m-2 shadow-lg bg-[#F0C8C2] rounded-2xl">
-            <header className="flex flex-row justify-between p-2 bg-light-hot rounded-t-2xl">
-              <div id="ticket-author" className="flex flex-row align-center">
-                <Image
-                  src={avatar}
-                  alt="avatar"
-                  className="w-10 h-10 rounded-full"
-                />
-                <p className="leading-10 ml-2">{ticket.name}</p>
-              </div>
-              <div
-                id="ticket-info"
-                className="flex flex-col text-right justify-right"
-              >
-                <h5>Ticket #{ticket.id}</h5>
-                <h4>Subject: {ticket.subject}</h4>
-              </div>
-            </header>
-            <main className="p-2">
-              <div>
-                <p>{ticket.lastMessage}</p>
-              </div>
-              <button
-                className="bg-light-hot rounded-lg px-2 py-1 mt-2 float-right"
-                // onClick={() => {
-                //   openTicket(ticket.id);
-                // }}
-              >
-                Read more
-              </button>
-            </main>
-          </article>
+          <>
+            <article className="flex flex-col max-w-lg m-2 shadow-lg bg-[#F0C8C2] rounded-2xl">
+              <header className="flex flex-row justify-between p-2 bg-light-hot rounded-t-2xl">
+                <div id="ticket-author" className="flex flex-row align-center">
+                  <Image
+                    src={avatar}
+                    alt="avatar"
+                    className="w-10 h-10 rounded-full"
+                  />
+                  <p className="leading-10 ml-2">{ticket.name}</p>
+                </div>
+                <div
+                  id="ticket-info"
+                  className="flex flex-col text-right justify-right"
+                >
+                  <h5>Ticket #{ticket.id}</h5>
+                  <h4>Subject: {ticket.subject}</h4>
+                </div>
+              </header>
+              <main className="p-2">
+                <div>
+                  <p>{ticket.lastMessage}</p>
+                </div>
+                <button
+                  className="bg-light-hot rounded-lg px-2 py-1 mt-2 float-right"
+                  // onClick={() => {
+                  //   openTicket(ticket.id);
+                  // }}
+                >
+                  Read more
+                </button>
+              </main>
+            </article>
+          </>
         );
       })}
     </>

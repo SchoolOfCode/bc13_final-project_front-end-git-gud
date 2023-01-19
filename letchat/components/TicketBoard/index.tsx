5392cb82c0e54ad6d35267183f9e103213c2d8c2
jasonjjjc <jason42@tuta.io>
2023-01-19 15:18:01 +0000

    subject shows on tickets

--- letchat/components/TicketBoard/index.tsx
+++ letchat/components/TicketBoard/index.tsx
@@ -10,18 +10,16 @@ export type TicketObject = {
   id: number;
   property_id: number;
   tenant_id: number;
+  request: string;
   completed: boolean;
   raised_by: string;
   first_name: string;
   last_name: string;
-  date: string;
-  time: string;
 };
 
 const TicketBoard = ({ completed }: CompletedProp) => {
   const [tickets, setTickets] = useState([]);
-
-  console.log(tickets);
+  // const [lastMessage, setLastMessage] = useState("");
 
   useEffect(() => {
     async function fetchTickets() {
@@ -30,28 +28,36 @@ const TicketBoard = ({ completed }: CompletedProp) => {
       setTickets(data.payload);
     }
     fetchTickets();
-  }, [completed]);
+  }, []);
 
-  // function getLastMessage(id: number) {
-  //   return fetch(`http://localhost:3001/api/messages/tickets/${id}`)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       let messageArrLength = data.payload.length;
-  //       let lastMessage = data.payload[messageArrLength - 1].message;
-  //       return lastMessage;
-  //     });
-  // }
-  // function callGetLastMessage(id: number) {
-  //   let lastMessage = getLastMessage(id);
-  //   .then();
-  //   return lastMessage;
-  // }
+  
 
   return (
     <div className="gap-3 overflow-y-scroll w-full flex flex-col items-center h-[60vh] text-black">
       {/* Map over tickets array, rendering each ticket */}
       {tickets.map((ticket: TicketObject) => {
-        // let lastMessage = callGetLastMessage(ticket.id);
+
+      // async function getLastMessage(id: number) {
+      //   let res = await fetch(`http://localhost:3001/api/messages/tickets/${id}`)
+      //   let data = await res.json();
+      //   let messageArrLength = await data.payload.length;
+      //   let lastMessageObj = await data.payload[messageArrLength - 1];
+      //   return lastMessageObj;
+      // }
+      
+      // async function getLastMessageText(id: number) {
+      //   let lastMessageObj = await getLastMessage(id);
+      //   let lastMessageText = await lastMessageObj.message;
+      //   return lastMessageText;
+      // }
+
+      // function callGetLastMessageText(id: number) {
+      //   let lastMessageText = getLastMessageText(id);
+      //   return lastMessageText;
+      // }
+
+      // let lastMessageText = callGetLastMessageText(ticket.id);
+
         return ticket.completed === completed ? (
           <Ticket
             key={ticket.id}
@@ -60,11 +66,10 @@ const TicketBoard = ({ completed }: CompletedProp) => {
             last_name={ticket.last_name}
             property_id={ticket.property_id}
             tenant_id={ticket.tenant_id}
+            request={ticket.request}
             raised_by={ticket.raised_by}
             completed={ticket.completed}
-            date={ticket.date}
-            time={ticket.time}
-            // lastMessage={lastMessage}
+            // lastMessage={lastMessageText}
           />
         ) : null;
       })}
@@ -73,3 +78,6 @@ const TicketBoard = ({ completed }: CompletedProp) => {
 };
 
 export default TicketBoard;
+
+
+

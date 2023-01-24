32d6f86cacd8a5b671d53d67b8c7f8a112a9859a
jasonjjjc <jason42@tuta.io>
2023-01-24 19:57:25 +0000

    restyled newsfeed

--- letchat/components/NewsFeed/index.tsx
+++ letchat/components/NewsFeed/index.tsx
@@ -14,21 +14,20 @@ const NewsFeed = ({
   email,
 }: CardContainerProps) => {
   return (
-    <div className="hidden lg:block max-h-[60vh] bg-white shadow-lg shadow-black rounded-lg  ">
-      <div className=" bg-light-primary  rounded-lg shadow-md shadow-light-tertiary h-full p-4 w-full">
-        <h4 className="md:text-[30px] text-white text-center bold uppercase font-bold ">
+    <div className="hidden lg:block h-[80vh] bg-white shadow-lg shadow-black rounded-lg  ">
+      <div className="  rounded-lg shadow-md shadow-light-tertiary  p-4 h-[50%]">
+        <h4 className="md:text-[30px] bg-light-primary text-white text-center bold uppercase font-bold ">
           News feed
         </h4>
+        <div className="flex flex-col flex-grow justify-between text-center rounded-bl-2xl rounded-br-2xl  bold uppercase mt-12 p-4 items-center ">
+          <ul>
+            <li>Landlord: Uploaded EPC</li>
+            <li>Tenant: Uploaded ID document</li>
+          </ul>
+        </div>
       </div>
-      <div className="flex flex-col justify-between text-center rounded-bl-2xl rounded-br-2xl  bold uppercase mt-12 p-4 items-center ">
-        <ul>
-          <li>Landlord: Uploaded EPC</li>
-          <li>Tenant: Uploaded ID document</li>
-        </ul>
-      </div>
-      <div>
-        <div className="md:text-[16px] flex-col flex justify-between text-center rounded-bl-2xl rounded-br-2xl  bold uppercase mt-12 p-4 items-center ">
-          <h2 className="md:text-[20px] text-center bold uppercase font-bold ">
+        <div className="md:text-[16px] flex-col flex justify-between text-center rounded-bl-2xl rounded-br-2xl  bold uppercase mt-12 p-4 items-center h-[50%] w-full">
+          <h2 className="md:text-[20px] text-center bold uppercase font-bold bg-light-secondary">
             {firstname} {surname}
           </h2>
           <p className="text-[15px] flex justify-between mb-2 w-[60%] items-center ">
@@ -37,7 +36,6 @@ const NewsFeed = ({
           <p className="text-[15px] ">Contact no: {phonenumber}</p>
           <p className="text-[15px] ">Email: {email}</p>
         </div>
-      </div>
 
       {/* <div className="shadow-lg shadow-black rounded-lg  ">
         <div className=" bg-light-primary rounded-lg shadow-md shadow-light-tertiary h-full p-4 w-full">

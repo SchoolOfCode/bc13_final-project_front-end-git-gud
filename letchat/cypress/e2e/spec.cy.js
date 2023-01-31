1f5022d989facbc9b9de28fe5fa74c9591fc4474
cowtipping <turbogerbil@gmail.com>
2023-01-31 12:44:17 +0000

    more Cypress tests

--- letchat/cypress/e2e/spec.cy.js
+++ letchat/cypress/e2e/spec.cy.js
@@ -1,4 +1,4 @@
-describe("Check empty input boxes spec", () => {
+describe("Check empty input boxes", () => {
   it("has a form which is empty", () => {
     cy.visit("http://localhost:3000");
     cy.get("#firstname").should("have.value", "");
@@ -27,4 +27,61 @@ describe("User can type", () => {
     cy.get("#password").type("Test Password");
     cy.get("#password").should("have.value", "Test Password");
   });
+});
+
+describe("Form submit", () => {
+  it("user cannot sign-up if they've submitted incorrect or missing information", () => {
+    cy.visit("http://localhost:3000");
+    cy.get("#phonenumber").type("123");
+    cy.get("#email").type("NotAnEmail");
+    cy.get("button[type=submit]").click();
+  });
+});
+
+describe("Footer links", () => {
+  it("has all the correct pages into the footer which link to the correct url", () => {
+    cy.visit("http://localhost:3000");
+
+    // about us
+    cy.contains("About us").click();
+    cy.location("pathname").should("eq", "/about");
+    cy.go("back");
+
+    // contact
+    cy.contains("Contact").click();
+    cy.location("pathname").should("eq", "/contact");
+    cy.go("back");
+
+    // terms of use
+    cy.contains("Terms of use").click();
+    cy.location("pathname").should("eq", "/termsofuse");
+    cy.go("back");
+
+    // privacy policy
+    cy.contains("Privacy policy").click();
+    cy.location("pathname").should("eq", "/privacypolicy");
+    cy.go("back");
+
+    // cookie policy
+    cy.contains("Cookie policy").click();
+    cy.location("pathname").should("eq", "/cookiepolicy");
+    cy.go("back");
+  });
+});
+
+describe("Log-in form", () => {
+  it(
+    "gets the login form and successfully signs in",
+    {
+      defaultCommandTimeout: 10000,
+    },
+    () => {
+      cy.visit("http://localhost:3000/login");
+      cy.get("#email").type(Cypress.env("cypress_email"));
+      cy.get("#password").type(Cypress.env("cypress_password"));
+      // TODO: error: constantly sends GET requests after log in
+      cy.get("button[type=submit]").click();
+      cy.location("pathname").should("eq", "/jobboard");
+    }
+  );
 });

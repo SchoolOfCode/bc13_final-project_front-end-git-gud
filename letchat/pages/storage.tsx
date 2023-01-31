af7e3ce2f05c56a8bbfa2c72b27a555ab3e76813
Bazelden <jack.waller@hotmail.com>
2023-01-31 18:09:26 +0000

    basic styling done

--- letchat/pages/storage.tsx
+++ letchat/pages/storage.tsx
@@ -10,6 +10,7 @@ import {
   deleteObject,
   getDownloadURL,
 } from "firebase/storage";
+import Container from "../components/Container";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +19,17 @@ export default function Storage() {
   const [fileList, setFileList] = useState<any>([]);
   const { user } = useAuth();
 
+  const fileListRef = ref(storage, `documents/${user.uid}`);
+
+  console.log(fileListRef);
+
   const uploadFile = () => {
     if (!file) return alert("No file selected");
     const fileRef = ref(storage, `documents/${user.uid}/${file.name}`);
     uploadBytes(fileRef, file).then((snapshot) => {
       alert("File uploaded successfully");
       getDownloadURL(snapshot.ref).then((url) => {
+        console.log(snapshot);
         setFileList((prevState: any) => [
           ...prevState,
           { name: snapshot.ref.name, fullPath: snapshot.ref.fullPath, url },
@@ -33,15 +39,15 @@ export default function Storage() {
   };
 
   useEffect(() => {
-    listAll(ref(storage, `documents/${user.uid}`)).then((res) => {
-      res.items.forEach((item) =>
+    listAll(fileListRef).then((res) => {
+      res.items.forEach((item) => {
         getDownloadURL(item).then((url) => {
           setFileList((prevState: any) => [
             ...prevState,
             { name: item.name, fullPath: item.fullPath, url },
           ]);
-        })
-      );
+        });
+      });
     });
   }, []);
 
@@ -66,31 +72,40 @@ export default function Storage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>
-        <h1>Document Storage</h1>
-        <p>
-          This is where you can store your documents. You can upload, download,
-          and delete documents.
-        </p>
-        <div>
-          <input
-            type="file"
-            onChange={(e: any) => setFile(e.target.files[0])}
-          />
-          <button onClick={uploadFile}>Upload</button>
+      <Container logo={true}>
+        <div className="flex flex-col items-center justify-center uppercase tracking-widest">
+          <h1>Document Storage</h1>
+          <p>
+            This is where you can store your documents. You can upload,
+            download, and delete documents.
+          </p>
+          <div className="py-8">
+            <input
+              type="file"
+              onChange={(e: any) => setFile(e.target.files[0])}
+            />
+            <button
+              className="my-2 h-10 w-full rounded-full border border-black bg-light-hot py-2 px-4 font-bold text-white shadow-xl shadow-gray-300 hover:border-transparent hover:bg-light-hot hover:opacity-60"
+              onClick={uploadFile}
+            >
+              Upload
+            </button>
+          </div>
         </div>
-        <ul>
-          {fileList.map((file: any) => (
-            <li key={file.name}>
-              <a href={file.url} target="_blank" rel="noreferrer">
-                {file.name}
-              </a>
-              <br />
-              <button onClick={() => deleteFile(file)}>Delete</button>
-            </li>
-          ))}
-        </ul>
-      </main>
+        <div>
+          <ul className="mt-[-10rem] flex flex-row justify-between">
+            {fileList.map((file: any) => (
+              <li key={file.name}>
+                <a href={file.url} target="_blank" rel="noreferrer">
+                  {file.name}
+                </a>
+                <br />
+                <button onClick={() => deleteFile(file)}>Delete</button>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Container>
     </>
   );
 }

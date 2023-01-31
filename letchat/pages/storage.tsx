70f998c5de5c6717bc9aef51192ef8919d9ef62e
Bazelden <jack.waller@hotmail.com>
2023-01-31 16:57:04 +0000

    file upload, deletion and download working but list is rendering two of the same item + styling to be done

--- letchat/pages/storage.tsx
+++ letchat/pages/storage.tsx
@@ -1,11 +1,58 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import { useAuth } from "../context/AuthContext";
+import { useState, useEffect } from "react";
+import { storage } from "../firebase/firebaseApp";
+import {
+  ref,
+  uploadBytes,
+  listAll,
+  deleteObject,
+  getDownloadURL,
+} from "firebase/storage";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Storage() {
+  const [file, setFile] = useState<File | null>(null);
+  const [fileList, setFileList] = useState<any>([]);
   const { user } = useAuth();
+
+  const uploadFile = () => {
+    if (!file) return alert("No file selected");
+    const fileRef = ref(storage, `documents/${user.uid}/${file.name}`);
+    uploadBytes(fileRef, file).then((snapshot) => {
+      alert("File uploaded successfully");
+      getDownloadURL(snapshot.ref).then((url) => {
+        setFileList((prevState: any) => [
+          ...prevState,
+          { name: snapshot.ref.name, fullPath: snapshot.ref.fullPath, url },
+        ]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(ref(storage, `documents/${user.uid}`)).then((res) => {
+      res.items.forEach((item) =>
+        getDownloadURL(item).then((url) => {
+          setFileList((prevState: any) => [
+            ...prevState,
+            { name: item.name, fullPath: item.fullPath, url },
+          ]);
+        })
+      );
+    });
+  }, []);
+
+  const deleteFile = (file: any) => {
+    deleteObject(ref(storage, file.fullPath)).then(() => {
+      alert("File deleted successfully");
+      setFileList((prevState: any) =>
+        prevState.filter((item: any) => item.fullPath !== file.fullPath)
+      );
+    });
+  };
 
   return (
     <>
@@ -19,7 +66,31 @@ export default function Storage() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main></main>
+      <main>
+        <h1>Document Storage</h1>
+        <p>
+          This is where you can store your documents. You can upload, download,
+          and delete documents.
+        </p>
+        <div>
+          <input
+            type="file"
+            onChange={(e: any) => setFile(e.target.files[0])}
+          />
+          <button onClick={uploadFile}>Upload</button>
+        </div>
+        <ul>
+          {fileList.map((file: any) => (
+            <li key={file.name}>
+              <a href={file.url} target="_blank" rel="noreferrer">
+                {file.name}
+              </a>
+              <br />
+              <button onClick={() => deleteFile(file)}>Delete</button>
+            </li>
+          ))}
+        </ul>
+      </main>
     </>
   );
 }

aa98e0fb3da135de67d6059f6558261f33f348db
jasonchalangary <jason42@tuta.io>
2023-03-07 08:39:06 +0000

    added loader to show data is loading on page load in ticketboard

--- letchat/components/TicketBoard/index.tsx
+++ letchat/components/TicketBoard/index.tsx
@@ -20,17 +20,27 @@ export type TicketObject = {
 
 const TicketBoard = ({ completed }: CompletedProp) => {
   const [tickets, setTickets] = useState<TicketObject[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [numLoads, setNumLoads] = useState<number>(0);
 
   async function fetchTickets() {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tickets/landlords/1`
-    );
-    const data = await res.json();
-    setTickets(data.payload);
+    setIsLoading(true);
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/tickets/landlords/1`
+      );
+      const data = await res.json();
+      setTickets(data.payload);
+      setIsLoading(false);
+    } catch (err) {
+      console.error(err);
+      setIsLoading(false);
+    }
   }
 
   useEffect(() => {
     fetchTickets();
+    setNumLoads(1);
   }, []);
 
   useEffect(() => {
@@ -71,23 +81,32 @@ const TicketBoard = ({ completed }: CompletedProp) => {
 
   return (
     <div className="grid max-h-[60vh] grid-cols-1 gap-3 overflow-y-scroll p-2 text-black md:max-h-[70vh] xl:grid-cols-2 2xl:grid-cols-3">
-      {tickets.map((ticket: TicketObject) => {
-        return ticket.completed === completed ? (
-          <Ticket
-            key={ticket.id}
-            id={ticket.id}
-            first_name={ticket.first_name}
-            last_name={ticket.last_name}
-            property_id={ticket.property_id}
-            tenant_id={ticket.tenant_id}
-            subject={ticket.subject}
-            message={ticket.message}
-            raised_by={ticket.raised_by}
-            completed={ticket.completed}
-            handleClick={(e: any) => handleClick(ticket.id, e.target.checked)}
-          />
-        ) : null;
-      })}
+      {isLoading ? (
+        <div className="flex flex-col min-w-[75vw] justify-center items-center">
+          <p>Loading...</p>
+        <div className="loader ease-linear rounded-full border-4 border-t-4 border-gray-200 h-12 w-12"></div>
+      </div>
+      ) : (
+        tickets.map((ticket: TicketObject) => {
+          return ticket.completed === completed ? (
+            <Ticket
+              key={ticket.id}
+              id={ticket.id}
+              first_name={ticket.first_name}
+              last_name={ticket.last_name}
+              property_id={ticket.property_id}
+              tenant_id={ticket.tenant_id}
+              subject={ticket.subject}
+              message={ticket.message}
+              raised_by={ticket.raised_by}
+              completed={ticket.completed}
+              handleClick={(e: any) =>
+                handleClick(ticket.id, e.target.checked)
+              }
+            />
+          ) : null;
+        })
+      )}
     </div>
   );
 };
